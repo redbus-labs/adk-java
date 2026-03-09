1cb4d431ca62573a8071b244e64971e6d60a42ad
Google Team Member <genai-sdk-bot@google.com>
2026-03-09 05:28:00 -0700

    refactor: Move RemoteA2AAgent to agent package; remove EXPERIMENTAL annotation; add LICENSE header
    PiperOrigin-RevId: 880804505

--- a2a/src/main/java/com/google/adk/a2a/converters/EventConverter.java
+++ a2a/src/main/java/com/google/adk/a2a/converters/EventConverter.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.adk.a2a.converters;
 
 import static com.google.common.collect.ImmutableList.toImmutableList;
@@ -13,12 +28,7 @@ import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * Converter for ADK Events to A2A Messages.
- *
- * <p>**EXPERIMENTAL:** Subject to change, rename, or removal in any future patch release. Do not
- * use in production code.
- */
+/** Converter for ADK Events to A2A Messages. */
 public final class EventConverter {
   private static final Logger logger = LoggerFactory.getLogger(EventConverter.class);
 
--- a2a/src/main/java/com/google/adk/a2a/converters/ResponseConverter.java
+++ a2a/src/main/java/com/google/adk/a2a/converters/ResponseConverter.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.adk.a2a.converters;
 
 import static com.google.common.collect.ImmutableList.toImmutableList;
@@ -27,12 +42,7 @@ import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * Utility for converting ADK events to A2A spec messages (and back).
- *
- * <p>**EXPERIMENTAL:** Subject to change, rename, or removal in any future patch release. Do not
- * use in production code.
- */
+/** Utility for converting ADK events to A2A spec messages (and back). */
 public final class ResponseConverter {
   private static final Logger logger = LoggerFactory.getLogger(ResponseConverter.class);
   private static final ImmutableSet<TaskState> PENDING_STATES =
